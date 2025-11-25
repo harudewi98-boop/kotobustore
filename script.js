83fa0a2e8474e2035bba0636c98ddf9316d0d2d0
harudewi98-boop <harudewi98@gmail.com>
2025-11-25 10:50:27 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Sama seperti sebelumnya (fungsi tombol "Beli Sekarang")
-const buyButtons = document.querySelectorAll('.buy-btn');
-
-buyButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        alert('Fitur ini belum tersedia. Silakan hubungi admin via WhatsApp.');
-    });
-});
